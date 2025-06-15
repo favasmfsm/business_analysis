7de31172b2757ce4b1a0d0b68cfa631455e86bf6
Favas M <favasmfsm@gmail.com>
2025-06-15 14:14:40 +0530

    purchase amount

--- app.py
+++ app.py
@@ -66,6 +66,16 @@ if df1 and df2:
             loose_oil_rate * 1.18
         )
 
+    # for all items in df with net_purchase_amount less than 10, ask the user to enter the net_purchase_amount
+    for index, row in df.iterrows():
+        if row["net_purchase_amount"] < 10:
+            net_purchase_amount = st.sidebar.number_input(
+                "Enter the net purchase amount for " + row["Description"],
+                value=row["net_purchase_amount"],
+                step=1,
+            )
+            df.at[index, "net_purchase_amount"] = net_purchase_amount
+
     df = df[~df["Customer Name"].str.contains("CASH A/C")]
     df["profit"] = df.Qty * (df["sale_rate"] - df.net_purchase_amount)
     df["profit_percentage"] = (
