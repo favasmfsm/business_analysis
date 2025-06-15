a9182a0533bf8630ca6397fa9ee71ede6c163043
Favas M <favasmfsm@gmail.com>
2025-06-15 14:18:03 +0530

    keys

--- app.py
+++ app.py
@@ -66,14 +66,17 @@ if df1 and df2:
             loose_oil_rate * 1.18
         )
 
-    # for all items in df with net_purchase_amount less than 10, ask the user to enter the net_purchase_amount
-    for index, row in df.iterrows():
-        if row["net_purchase_amount"] < 10:
+    # for unique description in df with net_purchase_amount less than 10, ask the user to enter the net_purchase_amount
+    for description in df["Description"].unique():
+        if df[df["Description"] == description]["net_purchase_amount"].min() < 10:
             net_purchase_amount = st.sidebar.text_input(
-                "Enter the net purchase amount for " + row["Description"],
-                value=row["net_purchase_amount"],
+                "Enter the net purchase amount for " + description,
+                value=df[df["Description"] == description]["net_purchase_amount"].min(),
+                key=description,
             )
-            df.at[index, "net_purchase_amount"] = float(net_purchase_amount)
+            df.loc[df["Description"] == description, "net_purchase_amount"] = float(
+                net_purchase_amount
+            )
 
     df = df[~df["Customer Name"].str.contains("CASH A/C")]
     df["profit"] = df.Qty * (df["sale_rate"] - df.net_purchase_amount)
